c14d5ecfa8602247b747fa8204330a418076a0ff
davidservia <david.servia.fp@iescampanillas.com>
2022-04-27 13:35:12 +0200

    Primer de con main vacio.

---
+++ src/controlador/Principal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package controlador;
+
+/**
+ * @author rapif
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
