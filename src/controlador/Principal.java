5a153f5921da6b996bd816f7bce42be57e912dc2
davidservia <david.servia.fp@iescampanillas.com>
2022-04-27 13:50:51 +0200

    cuarto commit

--- src/controlador/Principal.java
+++ src/controlador/Principal.java
@@ -20,4 +20,32 @@ public class Principal {
 
 	}
 
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * 
+	 */
+	public Principal() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Principal [nombre=" + nombre + ", getNombre()=" + getNombre() + ", getClass()=" + getClass()
+				+ ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+	}
+
 }
