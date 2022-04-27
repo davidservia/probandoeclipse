15e2d0e1887ee2410346f16bede799c57983ad69
davidservia <david.servia.fp@iescampanillas.com>
2022-04-27 13:44:07 +0200

    tercer commit con una clase nueva.

--- src/controlador/Principal.java
+++ src/controlador/Principal.java
@@ -8,12 +8,15 @@ package controlador;
  *
  */
 public class Principal {
+	
+	String nombre = "David ";
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("hola mundo");
 
 	}
 
